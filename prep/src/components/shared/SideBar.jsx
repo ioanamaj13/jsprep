2570468eedd086536a31743e6cb4453647d9d1bc
ioanamaj13 <iasauleac@gmail.com>
2022-08-22 21:59:21 +0100

    add some minimal styling

--- prep/src/components/shared/SideBar.jsx
+++ prep/src/components/shared/SideBar.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styles from "./sidebar.module.scss";
 
 export const SideBar = () => {
     return (
-        <aside className="sideMenu">
-            <h1>Header here</h1>
-            <ul>
-                <li><Link to="/">Home</Link></li>
+        <aside className={styles.sideMenu}>
+            <h1> <Link to="/">Home</Link></h1>
+            <ul className={styles.menuItem}>
                 <li><Link to="/fizzbuzz">FizzBuzz</Link></li>
                 <li><Link to="/palindrome">Palindrome</Link></li>
                 <li><Link to="/brackets">Brackets</Link></li>
-                <li> Pokemon
+                <li> <span className={styles.section}> Pokemon: </span>
                     <ul>
                         <li><Link to="/pokefetch">Fetch</Link></li>
                         <li><Link to="/pokeaxios">Axios</Link></li>
